3fa09973f79d250fa0db06aed9899bf76c990122
Ricardo <rt73@njit.edu>
2016-06-17 09:45:28 -0400

    Added small tag

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('Not Found@');
     err.status = 404;
     next(err);
 });
