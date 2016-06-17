118d3bd8521b32bf05ed12281286eba204d8a416
Ricardo <rt73@njit.edu>
2016-06-17 09:38:59 -0400

    changed routing again

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(multer()); // for parsing multipart/form-data
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/Colleges', routes);
+app.use('/', routes);
 
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
