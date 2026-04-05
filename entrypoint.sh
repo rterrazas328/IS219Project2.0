9af609c9b166cfef35f8d200572c29554dec81a5
rterrazas328 <rickyt328@gmail.com>
2026-04-05 02:00:57 -0400

    Fixed docker secret definition in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export DB_USER=`cat /run/secrets/db_user`
-export DB_PASSWORD=`cat /run/secrets/db_password`
+export DB_USER=$(cat /run/secrets/db_user)
+export DB_PASSWORD=$(cat /run/secrets/db_password)
 
 exec "$@"
