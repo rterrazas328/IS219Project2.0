3c1f5dc45cfaa2d04f0b2c2ed1ce3decf13922e8
rterrazas328 <rickyt328@gmail.com>
2026-04-08 19:48:41 -0400

    Added check to disable setting variables when using Railway

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-export DB_USER=$(cat /run/secrets/db_user)
-export DB_PASSWORD=$(cat /run/secrets/db_password)
+if [[ -v "$RAILWAY_SERVICE_ID" ]]; then
+
+else
+    export DB_USER=$(cat /run/secrets/db_user)
+    export DB_PASSWORD=$(cat /run/secrets/db_password)
+fi
+
 
 exec "$@"
